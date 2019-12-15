c0bc57e9a1bbf9467f10a1e225d4a70f12cddd1e
Gustavo Bicalho <gusbicalho@gmail.com>
2019-12-15 23:40:22 +0000

    begin implementing a custom effect for WaiApplication

--- src/EffectsToy.hs
+++ src/EffectsToy.hs
@@ -1,46 +1,23 @@
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# OPTIONS_GHC -Wno-partial-type-signatures #-}
 module EffectsToy
-    ( start
-    ) where
-
-import Control.Carrier.Reader
-import Control.Carrier.State.Strict
-import Control.Carrier.Writer.Strict
-import Control.Carrier.Lift
+  ( start
+  ) where
 
 import qualified Network.Wai as Wai
 import qualified Network.HTTP.Types as HTTP
 import qualified Network.Wai.Handler.Warp as Warp
-import qualified Data.ByteString.Streaming as Streaming
-import           Data.Functor.Of ( Of(..))
 
-type ByteStream = Streaming.ByteString IO
+import EffectsToy.Carrier.WaiApplication
 
 start :: IO ()
 start = Warp.run 8080 (runWaiApplication helloWorld)
 
-runWaiApplication :: _ () -> Wai.Application
-runWaiApplication action request respond = do
-  result <-
-    Streaming.toLazy
-    . runM @ByteStream
-    . runReader @Wai.Request request
-    . runWriter @HTTP.ResponseHeaders
-    . runState @HTTP.Status HTTP.status500
-    $ action
-  let (respBody :> (headers, (status, ()))) = result
-  respond (Wai.responseLBS status headers respBody)
-
-helloWorld :: ( Has (Reader Wai.Request) sig m
-              , Has (State HTTP.Status) sig m
-              , Has (Writer HTTP.ResponseHeaders) sig m
-              , Has (Lift ByteStream) sig m
-              )
+helloWorld :: ( Has WaiApplication sig m )
            => m ()
 helloWorld = do
-  req <- ask @Wai.Request
-  tell @HTTP.ResponseHeaders [(HTTP.hContentType, "text/plain")]
-  sendM @ByteStream "Hello, world!\n"
-  sendM @ByteStream ("You requested " <> Streaming.fromStrict (Wai.rawQueryString req))
-  put @HTTP.Status HTTP.ok200
+  req <- askRequest
+  tellHeaders [(HTTP.hContentType, "text/plain")]
+  sendChunk "Hello, world!\n"
+  sendChunk ("You requested " <> (Wai.rawQueryString req))
+  putStatus HTTP.ok200
---
+++ src/EffectsToy/Carrier/WaiApplication.hs
@@ -0,0 +1,94 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-partial-type-signatures #-}
+module EffectsToy.Carrier.WaiApplication
+  ( WaiApplication (..)
+  , askRequest, tellHeaders, putStatus, sendChunk
+  -- Carrier
+  , WaiApplicationC, runWaiApplicationC
+  , runWaiApplication
+  , Has
+  ) where
+
+import GHC.Generics (Generic1)
+
+import Control.Algebra
+import qualified Network.Wai as Wai
+import qualified Network.HTTP.Types as HTTP
+import qualified Data.ByteString as BS
+
+import Control.Carrier.Reader
+import Control.Carrier.State.Strict
+import Control.Carrier.Writer.Strict
+import Control.Carrier.Lift
+import qualified Data.ByteString.Streaming as Streaming
+import           Data.Functor.Of ( Of(..))
+
+
+
+-- Effect interface
+
+data WaiApplication m k
+  = AskRequest (Wai.Request -> m k)
+  | TellHeaders HTTP.ResponseHeaders (m k)
+  | PutStatus HTTP.Status (m k)
+  | SendChunk BS.ByteString (m k)
+  deriving (Generic1, Functor, HFunctor, Effect)
+
+askRequest :: (Has WaiApplication sig m) => m Wai.Request
+askRequest = send $ AskRequest pure
+
+tellHeaders :: (Has WaiApplication sig m) => HTTP.ResponseHeaders -> m ()
+tellHeaders headers = send $ TellHeaders headers (pure ())
+
+putStatus :: (Has WaiApplication sig m) => HTTP.Status -> m ()
+putStatus status = send $ PutStatus status (pure ())
+
+sendChunk :: (Has WaiApplication sig m) => BS.ByteString -> m ()
+sendChunk chunk = send $ SendChunk chunk (pure ())
+
+-- Carrier
+
+newtype WaiApplicationC m a = WaiApplicationC {
+  runWaiApplicationC :: StateC
+                          HTTP.Status
+                          (WriterC
+                            HTTP.ResponseHeaders
+                            (ReaderC
+                              Wai.Request
+                              (LiftC
+                                (Streaming.ByteString m))))
+                          a
+  } deriving newtype (Functor, Applicative, Monad)
+
+(<<) :: Monad m => m a -> m b -> m a
+(<<) = flip (>>)
+
+instance ( Algebra sig m
+         , Effect sig
+         ) => Algebra (WaiApplication :+: sig) (WaiApplicationC m) where
+  alg (L (AskRequest k))          = k =<< WaiApplicationC (ask)
+  alg (L (TellHeaders headers k)) = k << WaiApplicationC (tell headers)
+  alg (L (PutStatus status k))    = k << WaiApplicationC (put status)
+  alg (L (SendChunk chunk k))     = k << WaiApplicationC (sendM @(Streaming.ByteString m) (Streaming.fromStrict chunk))
+  alg (R other) = send other
+  {-# INLINE alg #-}
+
+handleWaiRequest :: Monad m => WaiApplicationC m () -> Wai.Request -> m Wai.Response
+handleWaiRequest waiApp request = do
+  result <-
+    Streaming.toLazy
+    . runM
+    . runReader @Wai.Request request
+    . runWriter @HTTP.ResponseHeaders
+    . runState @HTTP.Status HTTP.status500
+    . runWaiApplicationC
+    $ waiApp
+  let (respBody :> (headers, (status, ()))) = result
+  return (Wai.responseLBS status headers respBody)
+
+runWaiApplication :: WaiApplicationC IO () -> Wai.Application
+runWaiApplication waiApp request respond = do
+  response <- handleWaiRequest waiApp request
+  respond response
